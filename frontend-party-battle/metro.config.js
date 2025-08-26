989a544882e4660be8f0998e571f1a47657bdaa2
Robin Nater <76495150+Calfur@users.noreply.github.com>
2025-08-26 16:38:38 +0800

    migrate metro.config.js to ts

--- frontend-party-battle/metro.config.js
+++ frontend-party-battle/metro.config.js
@@ -1,19 +1,2 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
-
-const config = getDefaultConfig(__dirname);
-
-// Add resolver fallbacks for Node.js modules that Colyseus tries to use
-config.resolver.fallback = {
-  ...config.resolver.fallback,
-  https: false,
-  http: false,
-  crypto: false,
-  stream: false,
-  buffer: false,
-  url: false,
-  querystring: false,
-  zlib: false,
-};
-
-module.exports = withNativeWind(config, { input: "./global.css" });
+require('tsx/cjs');
+module.exports = require('./metro.config.ts');
