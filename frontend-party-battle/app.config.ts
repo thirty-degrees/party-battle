35ca59e395cc5a0ff2381e2a3f507fb958122de4
Robin Nater <76495150+Calfur@users.noreply.github.com>
2025-08-27 02:23:12 +0800

    feat: create android aab pipeline

--- frontend-party-battle/app.config.ts
+++ frontend-party-battle/app.config.ts
@@ -19,6 +19,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   android: {
     ...(config.android ?? {}),
+    package: "ch.thirty_degrees.party_battle",
     adaptiveIcon: {
       foregroundImage: "./assets/images/adaptive-icon.png",
       backgroundColor: "#ffffff",
