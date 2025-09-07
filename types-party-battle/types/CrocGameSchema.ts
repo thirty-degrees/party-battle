8e3f5d4f32795ac60427955389f4c47feead630a
damian <damian@web-hug.ch>
2025-09-07 23:43:57 +0800

    updated eas.json

--- types-party-battle/types/CrocGameSchema.ts
+++ types-party-battle/types/CrocGameSchema.ts
@@ -1,4 +1,7 @@
+import { ArraySchema, type } from "@colyseus/schema";
 import { GameSchema } from "./GameSchema";
 
 export class CrocGameSchema extends GameSchema {
+  @type("number") teethCount: number = 0;
+  @type(["number"]) pressedTeethIndex = new ArraySchema<number>();
 }
