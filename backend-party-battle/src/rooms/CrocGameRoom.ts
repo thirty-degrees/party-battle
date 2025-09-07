7c088e7e322ac8252f39eb97321366d314a015aa
damian <damian@web-hug.ch>
2025-09-08 03:51:46 +0800

    added game end when the hot tooth is pressed (croc-game)

--- backend-party-battle/src/rooms/CrocGameRoom.ts
+++ backend-party-battle/src/rooms/CrocGameRoom.ts
@@ -4,6 +4,7 @@ import { BaseGameRoom } from "../games/BaseGameRoom";
 export class CrocGameRoom extends BaseGameRoom<CrocGameSchema> {
   static readonly gameType: GameType = "croc";
   static readonly roomName: string = "croc_game_room";
+  hotToothIndex = 0;
 
   override getGameType(): GameType {
     return CrocGameRoom.gameType;
@@ -12,13 +13,16 @@ export class CrocGameRoom extends BaseGameRoom<CrocGameSchema> {
   override onCreate(options: { lobbyRoomId: string, playerNames: string[] }) {
     super.onCreate(options);
     this.state = new CrocGameSchema("waiting");
+    this.hotToothIndex = Math.floor(Math.random() * 12);
     this.state.teethCount = 12;
 
     this.onMessage("tooth_pressed", (client, message: { index: number }) => {
-      console.log(`Tooth ${message.index} pressed by client ${client.id}`);
       this.state.pressedTeethIndex.push(message.index);
+      
+      if (message.index === this.hotToothIndex) {
+        this.finishGame(options);
+      }
     });
-
   }
 
   override getScores(): Score[] {
