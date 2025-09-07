ccd12f7818e7a5314b732df2bece1fba42d8713e
damian <damian@web-hug.ch>
2025-09-08 04:27:54 +0800

    added a kinda working croc-game

--- frontend-party-battle/src/games/croc/CrocGame.tsx
+++ frontend-party-battle/src/games/croc/CrocGame.tsx
@@ -2,6 +2,7 @@ import { Room } from 'colyseus.js'
 import { Text, View } from 'react-native'
 import { CrocGameSchema } from 'types-party-battle'
 import useColyseusState from '../../colyseus/useColyseusState'
+import { usePlayerName } from '../../index/PlayerNameProvider'
 import ToothButtons from './ToothButtons'
 
 type CrocGameProps = {
@@ -12,9 +13,17 @@ export default function CrocGame({ gameRoom }: CrocGameProps) {
   const gameStatus = useColyseusState(gameRoom, (state) => state.status)
   const teethCount = useColyseusState(gameRoom, (state) => state.teethCount)
   const pressedTeethIndex = useColyseusState(gameRoom, (state) => Array.from(state.pressedTeethIndex || []))
+  const currentPlayer = useColyseusState(gameRoom, (state) => state.currentPlayer)
+  const inGamePlayers = useColyseusState(gameRoom, (state) => Array.from(state.inGamePlayers || []))
+  const { playerName } = usePlayerName()
+
+  const isCurrentPlayer = currentPlayer === playerName
+  const isPlayerInGame = inGamePlayers.some((player) => player.name === playerName)
 
   const handleToothPress = (toothIndex: number) => {
-    gameRoom.send('tooth_pressed', { index: toothIndex })
+    if (isCurrentPlayer) {
+      gameRoom.send('tooth_pressed', { index: toothIndex })
+    }
   }
 
   return (
@@ -22,6 +31,18 @@ export default function CrocGame({ gameRoom }: CrocGameProps) {
       <Text className="text-black dark:text-white text-2xl font-bold">Croc Mini Game</Text>
       <Text className="text-black dark:text-white text-lg">Game Status: {gameStatus}</Text>
       <Text className="text-black dark:text-white text-lg">Teeth Count: {teethCount}</Text>
+
+      {!isPlayerInGame && (
+        <Text className="text-red-500 dark:text-red-400 text-2xl font-bold">You are OUT</Text>
+      )}
+
+      {isCurrentPlayer && isPlayerInGame && (
+        <Text className="text-green-500 dark:text-green-400 text-xl font-bold">It&apos;s your turn!</Text>
+      )}
+
+      {!isCurrentPlayer && currentPlayer && isPlayerInGame && (
+        <Text className="text-gray-500 dark:text-gray-400 text-lg">{currentPlayer}&apos;s turn</Text>
+      )}
 
       <ToothButtons
         teethCount={teethCount}
--- types-party-battle/types/CrocGameSchema.ts
+++ types-party-battle/types/CrocGameSchema.ts
@@ -6,4 +6,5 @@ export class CrocGameSchema extends GameSchema {
   @type("number") teethCount: number = 0;
   @type(["number"]) pressedTeethIndex = new ArraySchema<number>();
   @type([PlayerSchema]) inGamePlayers = new ArraySchema<PlayerSchema>();
+  @type("string") currentPlayer: string = "";
 }
