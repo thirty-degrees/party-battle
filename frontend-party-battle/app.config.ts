1f079b7d375f331dabbdf96095b82771050a20e4
Robin Nater <76495150+Calfur@users.noreply.github.com>
2025-09-09 23:16:52 +0800

    feat: improve environment variable usage

--- frontend-party-battle/app.config.ts
+++ frontend-party-battle/app.config.ts
@@ -1,75 +1,98 @@
 import { ConfigContext, ExpoConfig } from 'expo/config'
 
-export default ({ config }: ConfigContext): ExpoConfig => ({
-  ...config,
-  name: 'Party Battle',
-  slug: 'frontend-party-battle',
-  orientation: 'portrait',
-  icon: './assets/images/icon.png',
-  scheme: 'partybattle',
-  userInterfaceStyle: 'automatic',
-  newArchEnabled: true,
-  ios: {
-    ...(config.ios ?? {}),
-    supportsTablet: true,
-    bundleIdentifier: 'ch.thirty-degrees.party-battle',
-    associatedDomains: ['applinks:party-battle.thirty-degrees.ch'],
-  },
-  android: {
-    ...(config.android ?? {}),
-    package: 'ch.thirty_degrees.party_battle',
-    adaptiveIcon: {
-      foregroundImage: './assets/images/adaptive-icon.png',
-      backgroundColor: '#ffffff',
+export default ({ config }: ConfigContext): ExpoConfig => {
+  const env = process.env.EXPO_PUBLIC_ENV || 'dev'
+  if (!['prod', 'dev'].includes(env)) throw new Error(`Invalid EXPO_PUBLIC_ENV: ${env}`)
+
+  const envMap = {
+    prod: {
+      backendUrl: `https://party-battle.thirty-degrees.ch/api/v${config.version}`,
+      frontendUrl: `https://party-battle.thirty-degrees.ch`,
+      owner: 'thirty-degrees',
+      easProjectId: '8907377f-75e3-4efd-9a3a-4556dfda4c47',
     },
-    edgeToEdgeEnabled: true,
-    intentFilters: [
-      {
-        action: 'VIEW',
-        autoVerify: true,
-        data: [{ scheme: 'https', host: 'party-battle.thirty-degrees.ch' }],
-        category: ['BROWSABLE', 'DEFAULT'],
-      },
-    ],
-  },
-  web: {
-    bundler: 'metro',
-    output: 'static',
-    favicon: './assets/images/favicon.png',
-  },
-  plugins: [
-    'expo-router',
-    [
-      'expo-splash-screen',
-      {
-        image: './assets/images/splash-icon.png',
-        imageWidth: 200,
-        resizeMode: 'contain',
+    dev: {
+      backendUrl: process.env.EXPO_PUBLIC_BACKEND_URL || `http://localhost:2567`,
+      frontendUrl: process.env.EXPO_PUBLIC_FRONTEND_URL || `http://localhost:8081`,
+      owner: process.env.EXPO_PUBLIC_OWNER,
+      easProjectId: process.env.EXPO_PUBLIC_EAS_PROJECT_ID,
+    },
+  } as const
+
+  const { backendUrl, frontendUrl, owner, easProjectId } = envMap[env as keyof typeof envMap]
+
+  return {
+    ...config,
+    name: 'Party Battle',
+    slug: 'frontend-party-battle',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'partybattle',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: true,
+    ios: {
+      ...(config.ios ?? {}),
+      supportsTablet: true,
+      bundleIdentifier: 'ch.thirty-degrees.party-battle',
+      associatedDomains: ['applinks:party-battle.thirty-degrees.ch'],
+    },
+    android: {
+      ...(config.android ?? {}),
+      package: 'ch.thirty_degrees.party_battle',
+      adaptiveIcon: {
+        foregroundImage: './assets/images/adaptive-icon.png',
         backgroundColor: '#ffffff',
       },
-    ],
-    [
-      'expo-build-properties',
-      {
-        android: {
-          enableProguardInReleaseBuilds: true,
-          enableShrinkResourcesInReleaseBuilds: true,
+      edgeToEdgeEnabled: true,
+      intentFilters: [
+        {
+          action: 'VIEW',
+          autoVerify: true,
+          data: [{ scheme: 'https', host: 'party-battle.thirty-degrees.ch' }],
+          category: ['BROWSABLE', 'DEFAULT'],
         },
-      },
-    ],
-  ],
-  experiments: {
-    typedRoutes: true,
-    reactCompiler: true,
-  },
-  extra: {
-    eas: {
-      projectId: '8907377f-75e3-4efd-9a3a-4556dfda4c47',
+      ],
     },
-    backendUrl:
-      process.env.EXPO_PUBLIC_BACKEND_URL || `https://party-battle.thirty-degrees.ch/api/v${config.version}`,
-    frontendUrl: process.env.EXPO_PUBLIC_FRONTEND_URL || `https://party-battle.thirty-degrees.ch`,
-    router: {},
-  },
-  owner: 'thirty-degrees',
-})
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/favicon.png',
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-splash-screen',
+        {
+          image: './assets/images/splash-icon.png',
+          imageWidth: 200,
+          resizeMode: 'contain',
+          backgroundColor: '#ffffff',
+        },
+      ],
+      [
+        'expo-build-properties',
+        {
+          android: {
+            enableProguardInReleaseBuilds: true,
+            enableShrinkResourcesInReleaseBuilds: true,
+          },
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+    extra: {
+      ...(easProjectId && {
+        eas: {
+          projectId: easProjectId,
+        },
+      }),
+      backendUrl: backendUrl,
+      frontendUrl: frontendUrl,
+      router: {},
+    },
+    ...(owner && { owner }),
+  }
+}
