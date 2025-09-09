6d63502a02d1b9c38a2f22b2d6ebe0c41ca2ca38
Robin Nater <76495150+Calfur@users.noreply.github.com>
2025-09-10 00:01:26 +0800

    refactor assignScoresByOrder

--- backend-party-battle/src/scores/assignScoresByOrder.ts
+++ backend-party-battle/src/scores/assignScoresByOrder.ts
@@ -1,19 +1,11 @@
 import { Score } from "types-party-battle";
 
 export function assignScoresByOrder(players: string[][]): Score[] {
-  const result: Score[] = [];
-  let currentScore = 0;
+  let offset = 0;
 
-  for (const rankGroup of players) {
-    const groupScore = currentScore + (rankGroup.length - 1);
-    for (const playerName of rankGroup) {
-      result.push({
-        playerName,
-        value: groupScore,
-      });
-    }
-    currentScore += rankGroup.length;
-  }
-
-  return result;
+  return players.flatMap((group) => {
+    const value = offset + group.length - 1;
+    offset += group.length;
+    return group.map((playerName) => ({ playerName, value }));
+  });
 }
