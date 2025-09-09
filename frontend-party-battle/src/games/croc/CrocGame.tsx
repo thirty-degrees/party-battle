4223070dd686e4f26cfff76ddc413e7aec12fab4
damian <damian@web-hug.ch>
2025-09-10 01:55:08 +0800

    updated ui slightly from crocGame

--- frontend-party-battle/src/games/croc/CrocGame.tsx
+++ frontend-party-battle/src/games/croc/CrocGame.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from 'react-native'
+import { Text } from '@/components/ui/text'
+import { View } from 'react-native'
 import { CrocGameSchema } from 'types-party-battle'
 import useColyseusState from '../../colyseus/useColyseusState'
 import { usePlayerName } from '../../index/PlayerNameProvider'
@@ -7,7 +8,6 @@ import TimerProgressBar from './TimerProgressBar'
 import ToothButtons from './ToothButtons'
 
 export const CrocGame: GameComponent<CrocGameSchema> = ({ gameRoom }) => {
-  const gameStatus = useColyseusState(gameRoom, (state) => state.status)
   const teethCount = useColyseusState(gameRoom, (state) => state.teethCount)
   const pressedTeethIndex = useColyseusState(gameRoom, (state) => Array.from(state.pressedTeethIndex || []))
   const currentPlayer = useColyseusState(gameRoom, (state) => state.currentPlayer)
@@ -25,34 +25,35 @@ export const CrocGame: GameComponent<CrocGameSchema> = ({ gameRoom }) => {
   }
 
   return (
-    <View className="flex-1 bg-white dark:bg-black justify-center items-center space-y-12 p-4">
-      <Text className="text-black dark:text-white text-2xl font-bold">Croc Mini Game</Text>
-      <Text className="text-black dark:text-white text-lg">Game Status: {gameStatus}</Text>
-      <Text className="text-black dark:text-white text-lg">Teeth Count: {teethCount}</Text>
-
-      {!isPlayerInGame && (
-        <Text className="text-red-500 dark:text-red-400 text-2xl font-bold">You are OUT</Text>
-      )}
-
-      {isCurrentPlayer && isPlayerInGame && (
-        <>
-          <Text className="text-green-500 dark:text-green-400 text-xl font-bold">It&apos;s your turn!</Text>
-          <TimerProgressBar
-            timeWhenTimerIsOver={timeWhenTimerIsOver}
-            isActive={isCurrentPlayer && isPlayerInGame}
-          />
-        </>
-      )}
-
-      {!isCurrentPlayer && currentPlayer && isPlayerInGame && (
-        <Text className="text-gray-500 dark:text-gray-400 text-lg">{currentPlayer}&apos;s turn</Text>
-      )}
-
-      <ToothButtons
-        teethCount={teethCount}
-        pressedTeethIndex={pressedTeethIndex}
-        onToothPress={handleToothPress}
-      />
+    <View className="flex-1 bg-white dark:bg-black p-4">
+      <View className="flex-1 items-center justify-center space-y-3">
+        <TimerProgressBar
+          timeWhenTimerIsOver={timeWhenTimerIsOver}
+          isActive={isCurrentPlayer && isPlayerInGame}
+        />
+        {!isPlayerInGame && (
+          <Text className="text-red-500 dark:text-red-400 text-2xl font-bold">You are OUT</Text>
+        )}
+        {isPlayerInGame && (
+          <>
+            {isCurrentPlayer && (
+              <Text className="text-green-500 dark:text-green-400 text-xl font-bold">
+                It&apos;s your turn!
+              </Text>
+            )}
+            {!isCurrentPlayer && currentPlayer && (
+              <Text className="text-gray-500 dark:text-gray-400 text-lg">{currentPlayer}&apos;s turn</Text>
+            )}
+          </>
+        )}
+      </View>
+      <View className="flex-1 items-center justify-center">
+        <ToothButtons
+          teethCount={teethCount}
+          pressedTeethIndex={pressedTeethIndex}
+          onToothPress={handleToothPress}
+        />
+      </View>
     </View>
   )
 }
