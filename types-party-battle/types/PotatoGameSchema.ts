2628bc05eb5de21ddcb24bddf97acf86fe6a3da7
Robin Nater <76495150+Calfur@users.noreply.github.com>
2025-09-09 15:50:07 +0800

    Update PotatoGameSchema.ts

--- types-party-battle/types/PotatoGameSchema.ts
+++ types-party-battle/types/PotatoGameSchema.ts
@@ -1,3 +1,8 @@
+import { ArraySchema, type } from "@colyseus/schema";
 import { GameSchema } from "./GameSchema";
 
-export class PotatoGameSchema extends GameSchema {}
+export class PotatoGameSchema extends GameSchema {
+  @type("string") playerWithPotato: string = "";
+  @type(["string"]) remainingPlayers = new ArraySchema<string>();
+  @type("string") message: string = "";
+}
