5b826075a3dc79fedf673a176b065345fab35818
Robin Nater <76495150+Calfur@users.noreply.github.com>
2025-09-16 17:19:45 +0800

    improve cell schema

--- types-party-battle/types/snake/Cell.ts
+++ types-party-battle/types/snake/Cell.ts
@@ -1,6 +1,17 @@
 import { Schema, type } from "@colyseus/schema";
 
+export enum CellKind {
+  Empty = 0,
+  Snake = 1,
+  Collectible = 2,
+}
+
 export class Cell extends Schema {
-  @type("uint8") kind: number = 0;
-  @type("string") playerId?: string;
+  @type("uint8") kind: CellKind;
+  @type("string") player?: string;
+
+  constructor(kind: CellKind) {
+    super();
+    this.kind = kind;
+  }
 }
