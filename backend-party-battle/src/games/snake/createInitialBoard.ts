a49e3e92d2e0b308a9a5a00369f0cc838fd92ca8
Robin Nater <76495150+Calfur@users.noreply.github.com>
2025-09-16 20:31:34 +0800

    feat: createInitialBoard

--- backend-party-battle/src/games/snake/createInitialBoard.ts
+++ backend-party-battle/src/games/snake/createInitialBoard.ts
@@ -13,5 +13,17 @@ export function createInitialBoard(playerNames: string[]): {
     board.push({ kind: CellKind.Empty })
   }
 
+  playerNames.forEach((playerName, playerIndex) => {
+    const y = 2 + playerIndex * 5
+    const isEven = playerIndex % 2 === 0
+    const xStart = isEven ? 0 : 16
+    const xEnd = isEven ? 3 : 19
+
+    for (let x = xStart; x <= xEnd; x++) {
+      const cellIndex = y * width + x
+      board[cellIndex] = { kind: CellKind.Snake, player: playerName }
+    }
+  })
+
   return { board, width, height }
 }
