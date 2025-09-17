ba25137e0fae3b24b013806d7afaa973137c964b
Robin Nater <76495150+Calfur@users.noreply.github.com>
2025-09-18 00:52:09 +0800

    fix: potato heat animation

--- frontend-party-battle/src/games/potato/PotatoStack.tsx
+++ frontend-party-battle/src/games/potato/PotatoStack.tsx
@@ -1,11 +1,11 @@
 import { View, ViewStyle } from 'react-native'
-import PotatoHeatSvg from './PotatoHeatSvg'
+import PotatoHeatImage from './PotatoHeatImage'
 import PotatoSvg from './PotatoSvg'
 
 export default function PotatoStack({ style }: { style?: ViewStyle }) {
   return (
     <View className="relative items-center justify-center z-0" style={style}>
-      <PotatoHeatSvg />
+      <PotatoHeatImage />
       <View className="absolute inset-0 items-center justify-center z-1">
         <PotatoSvg />
       </View>
