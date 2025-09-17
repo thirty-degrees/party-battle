c1ddc815d486c4fcde42a1a2738176b6a61438f2
Robin Nater <76495150+Calfur@users.noreply.github.com>
2025-09-17 16:46:36 +0800

    readd lobby

--- frontend-party-battle/app/index.tsx
+++ frontend-party-battle/app/index.tsx
@@ -14,6 +14,7 @@ import { usePlayerName } from '@/src/index/PlayerNameProvider'
 import { StoreBadges } from '@/src/index/StoreBadges'
 import { useLobbyRoomContext } from '@/src/lobby/LobbyRoomProvider'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { PLAYER_NAME_MAX_LENGTH } from 'types-party-battle/consts/config'
 
 export default function HomeScreen() {
   const [showJoinModal, setShowJoinModal] = useState(false)
@@ -78,7 +79,7 @@ export default function HomeScreen() {
                 onChangeText={onChangePlayerName}
                 placeholder="Enter your name..."
                 autoComplete="username"
-                maxLength={15}
+                maxLength={PLAYER_NAME_MAX_LENGTH}
                 style={{ width: 200, textAlign: 'center' }}
               />
             </Input>
