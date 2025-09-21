09706f555919387b02ae4f95e87dd7772d88f757
Robin Nater <76495150+Calfur@users.noreply.github.com>
2025-09-22 00:37:47 +0800

    refactor: snake update

--- backend-party-battle/src/games/snake/__tests__/getMovementIntentions.test.ts
+++ backend-party-battle/src/games/snake/__tests__/getMovementIntentions.test.ts
@@ -1,13 +1,13 @@
 import { Direction, RemainingPlayer } from 'types-party-battle/types/snake/RemainingPlayerSchema'
-import { getIntentions, MovementIntention } from '../getIntentions'
+import { getMovementIntentions, MovementIntention } from '../getMovementIntentions'
 
-describe('getIntentions', () => {
+describe('getMovementIntentions', () => {
   describe('when there is 1 snake with length 2 moving right', () => {
     const remainingPlayers: RemainingPlayer[] = [{ name: '1', direction: Direction.Right }]
     const bodies = new Map([['1', [3, 4]]])
     const width = 3
 
-    const intentions = getIntentions(remainingPlayers, bodies, width)
+    const intentions = getMovementIntentions(remainingPlayers, bodies, width)
 
     it('should create correct intentions', () => {
       expect(intentions).toEqual<MovementIntention[]>([{ name: '1', head: { x: 2, y: 1 }, tail: 3 }])
@@ -19,7 +19,7 @@ describe('getIntentions', () => {
     const bodies = new Map([['1', [2]]])
     const width = 2
 
-    const intentions = getIntentions(remainingPlayers, bodies, width)
+    const intentions = getMovementIntentions(remainingPlayers, bodies, width)
 
     it('should create correct intentions', () => {
       expect(intentions).toEqual<MovementIntention[]>([{ name: '1', head: { x: 1, y: 1 }, tail: 2 }])
@@ -37,7 +37,7 @@ describe('getIntentions', () => {
     ])
     const width = 3
 
-    const intentions = getIntentions(remainingPlayers, bodies, width)
+    const intentions = getMovementIntentions(remainingPlayers, bodies, width)
 
     it('should create correct intentions', () => {
       expect(intentions).toEqual<MovementIntention[]>([
