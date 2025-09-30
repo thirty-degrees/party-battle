c1124ba24443bfed485c45b7855bbcbed38a4134
Robin Nater <76495150+Calfur@users.noreply.github.com>
2025-09-30 22:19:27 +0800

    fix: to many colors

--- types-party-battle/consts/config.ts
+++ types-party-battle/consts/config.ts
@@ -6,14 +6,13 @@ const color = (r: number, g: number, b: number): RGBColor => ({ r, g, b });
 
 export const COLOR_NAME_TO_RGB = {
   red: color(229, 62, 62),
-  blue: color(49, 130, 206),
-  green: color(56, 161, 105),
   orange: color(221, 107, 32),
-  darkgreen: color(0, 100, 0),
   lightgreen: color(144, 238, 144),
+  darkgreen: color(59, 103, 36),
+  lightblue: color(34, 211, 238),
+  blue: color(49, 130, 206),
   purple: color(128, 90, 213),
   pink: color(213, 63, 140),
-  cyan: color(34, 211, 238),
 } as const;
 
 export type ColorName = keyof typeof COLOR_NAME_TO_RGB;
