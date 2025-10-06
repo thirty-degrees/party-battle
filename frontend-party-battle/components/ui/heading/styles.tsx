e9a088090a02d372f0604909ce2af6a1ab9ceb2b
Robin Nater <76495150+Calfur@users.noreply.github.com>
2025-10-06 23:13:32 +0800

    fix: heading font on Android

--- frontend-party-battle/components/ui/heading/styles.tsx
+++ frontend-party-battle/components/ui/heading/styles.tsx
@@ -1,10 +1,10 @@
-import { tva , isWeb } from '@gluestack-ui/utils/nativewind-utils';
+import { isWeb, tva } from '@gluestack-ui/utils/nativewind-utils'
 const baseStyle = isWeb
   ? 'font-sans tracking-sm bg-transparent border-0 box-border display-inline list-none margin-0 padding-0 position-relative text-start no-underline whitespace-pre-wrap word-wrap-break-word'
-  : '';
+  : ''
 
 export const headingStyle = tva({
-  base: `text-typography-900 dark:text-typography-100 font-bold font-heading tracking-sm my-0 ${baseStyle}`,
+  base: `text-typography-900 dark:text-typography-100 font-heading font-normal tracking-sm my-0 ${baseStyle}`,
   variants: {
     isTruncated: {
       true: 'truncate',
@@ -32,11 +32,11 @@ export const headingStyle = tva({
       '4xl': 'text-5xl',
       '3xl': 'text-4xl',
       '2xl': 'text-3xl',
-      'xl': 'text-2xl',
-      'lg': 'text-xl',
-      'md': 'text-lg',
-      'sm': 'text-base',
-      'xs': 'text-sm',
+      xl: 'text-2xl',
+      lg: 'text-xl',
+      md: 'text-lg',
+      sm: 'text-base',
+      xs: 'text-sm',
     },
   },
-});
+})
